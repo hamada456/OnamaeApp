e8cbc528a6d4a4110af750cb1c661360d95a1153
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-12-07 17:04:56 +0900

    完成

--- app/src/main/java/com/example/onamaeapp/MainActivity.kt
+++ app/src/main/java/com/example/onamaeapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.onamaeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val et:EditText = findViewById(R.id.et)
+        val btnStart: Button = findViewById(R.id.btnStart)
+
+        btnStart.setOnClickListener {
+            val intent = Intent(this,SecondActivity::class.java)
+            //値を渡す val et:EditText = "A"
+            intent.putExtra("MY_NAME",et.text.toString())
+
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/onamaeapp/SecondActivity.kt
+++ app/src/main/java/com/example/onamaeapp/SecondActivity.kt
@@ -2,10 +2,23 @@ package com.example.onamaeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val tvName :TextView = findViewById(R.id.tvName)
+        val btnBack: Button = findViewById(R.id.btnBack)
+
+        //値を取り出す
+        val myName = intent.getStringExtra("MY_NAME")
+        tvName.text = myName + "さん"
+
+        btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
